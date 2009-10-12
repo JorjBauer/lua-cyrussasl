d368b3d0108a19aae0080837cf22f4ab5755a3af
Jorj Bauer <jorj@jorj.org>
2009-10-12 11:23:16 -0400

    cleaned up abstraction

--- luaabstract.h
+++ luaabstract.h
@@ -3,11 +3,7 @@
 
 #include <lua.h>
 
-void expected(lua_State *l, const char *exp, int index);
-lua_Integer tointeger(lua_State *l, int index);
 const char *tolstring(lua_State *l, int index, size_t *len);
 const char *tostring(lua_State *l, int index);
-void *tolightuserdata(lua_State *l, int index);
-void declfunc(lua_State *l, const char *name, lua_CFunction func);
 
 #endif
