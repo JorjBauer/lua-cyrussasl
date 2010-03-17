7402bb86cc3982a097c22d84c0ec6b849a803498
Paul Aurich <paul@darkrain42.org>
2010-03-17 12:05:59 -0700

    bug fix: lua_typename takes a type, not a stack index

--- luaabstract.c
+++ luaabstract.c
@@ -5,11 +5,12 @@
 
 const char *tolstring(lua_State *l, int index, size_t *len)
 {
-  if (lua_type(l, index) != LUA_TSTRING) {
+  int type = lua_type(l, index);
+  if (type != LUA_TSTRING) {
     char err[256];
     snprintf(err, sizeof(err),
 	     "expected string, got %s",
-	     lua_typename(l, index));
+	     lua_typename(l, type));
     lua_pushstring(l, err);
     lua_error(l);
     return NULL;
@@ -28,11 +29,12 @@ const char *tostring(lua_State *l, int index)
 
 int tointeger(lua_State *l, int index)
 {
-  if (lua_type(l, index) != LUA_TNUMBER) {
+  int type = lua_type(l, index);
+  if (type != LUA_TNUMBER) {
     char err[256];
     snprintf(err, sizeof(err),
 	     "expected integer, got %s",
-	     lua_typename(l, index));
+	     lua_typename(l, type));
     lua_pushstring(l, err);
     lua_error(l);
     return 0;
